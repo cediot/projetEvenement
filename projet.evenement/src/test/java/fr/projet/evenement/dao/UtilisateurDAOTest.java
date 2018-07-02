1aad61d8a630b811765da38fb21050024803ddb1
florian <florian-lec@hotmail.fr>
2018-07-02 16:02:31 +0200

    modification du dernier test

--- projet.evenement/src/test/java/fr/projet/evenement/dao/UtilisateurDAOTest.java
+++ projet.evenement/src/test/java/fr/projet/evenement/dao/UtilisateurDAOTest.java
@@ -72,9 +72,9 @@ public class UtilisateurDAOTest extends AbstractAnnotationTest {
 
 	@Test
 	public void ComparateurUtil() throws Exception {
-		Utilisateur comparateurUtil = this.dao.findOne(8);
-		Utilisateur comparateurUtilDeux = this.dao.findOne(9);
-		Assert.assertNotEquals("verifique qu'il n'y a pas deux utilisateur identique", comparateurUtil.getNom(),
+		Utilisateur comparateurUtil = this.dao.save(this.createUtilisateur());
+		Utilisateur comparateurUtilDeux = this.dao.save(this.createUtilisateur());
+		Assert.assertEquals("verifique qu'il n'y a pas deux utilisateur identique", comparateurUtil.getNom(),
 				comparateurUtilDeux.getNom());
 	}
 }
