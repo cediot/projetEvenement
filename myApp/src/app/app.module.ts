08ad4b2778d119a29c3ad8eb34dca8076256866a
LeK59 <khalid.akhlafa@gmail.com>
2018-07-04 10:35:46 +0200

    premier commit pour myApp

---
+++ myApp/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { SettingsPage } from '../pages/Settings/Settings';
+import { AjouterPage } from '../pages/Ajouter/Ajouter';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    SettingsPage,
+    AjouterPage,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    SettingsPage,
+    AjouterPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    
+  ]
+})
+export class AppModule { }
