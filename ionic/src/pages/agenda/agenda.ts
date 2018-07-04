223f9bdcdc8b196665c05d7a23549de7be57c579
Jin81 <jingho@hotmail.com>
2018-07-04 10:22:51 +0200

    last update

--- ionic/src/pages/agenda/agenda.ts
+++ ionic/src/pages/agenda/agenda.ts
@@ -15,15 +15,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AgendaPage {
 
-  date: any;
-  daysInThisMonth: any;
-  daysInLastMonth: any;
-  daysInNextMonth: any;
-  monthNames: string[];
-  currentMonth: any;
-  currentYear: any;
-  currentDate: any;
- 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
    
@@ -33,49 +24,6 @@ export class AgendaPage {
     console.log('ionViewDidLoad AgendaPage');
   }
 
-  getDaysOfMonth() {
-    this.daysInThisMonth = new Array();
-    this.daysInLastMonth = new Array();
-    this.daysInNextMonth = new Array();
-    this.currentMonth = this.monthNames[this.date.getMonth()];
-    this.currentYear = this.date.getFullYear();
-    if (this.date.getMonth() === new Date().getMonth()) {
-      this.currentDate = new Date().getDate();
-    } else {
-      this.currentDate = 1999;
-    }
-
-    let firstDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1).getDay();
-    let prevNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate();
-    for (let i = prevNumOfDays - (firstDayThisMonth - 1); i <= prevNumOfDays; i++) {
-      this.daysInLastMonth.push(i);
-    }
-
-    let thisNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate();
-    for (let i = 0; i < thisNumOfDays; i++) {
-      this.daysInThisMonth.push(i + 1);
-    }
-
-    let lastDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDay();
-    let nextNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth() + 2, 0).getDate();
-    for (let i = 0; i < (6 - lastDayThisMonth); i++) {
-      this.daysInNextMonth.push(i + 1);
-    }
-    let totalDays = this.daysInLastMonth.length + this.daysInThisMonth.length + this.daysInNextMonth.length;
-    if (totalDays < 36) {
-      for (let i = (7 - lastDayThisMonth); i < ((7 - lastDayThisMonth) + 7); i++) {
-        this.daysInNextMonth.push(i);
-      }
-    }
-  }
-  goToLastMonth() {
-    this.date = new Date(this.date.getFullYear(), this.date.getMonth(), 0);
-    this.getDaysOfMonth();
-  }
   
-  goToNextMonth() {
-    this.date = new Date(this.date.getFullYear(), this.date.getMonth()+2, 0);
-    this.getDaysOfMonth();
-  }
  
   }
